77240a55272291d90cb51c8e09a7b30deb242f07
Melinda Ivanov <jns.melinda@gmail.com>
2020-11-29 18:40:22 -0800

    get route to get a book by id from db

--- routes/api.js
+++ routes/api.js
@@ -18,6 +18,16 @@ router.get("/api/books", (req, res) => {
 });
 
 router.get("/api/books/:id", (req, res) => {
+  Books.findById({_id: req.params.id})
+    .then(book => {
+      res.json(book);
+    })
+    .catch(err => {
+      res.status(404).json(err);
+    });
+});
+
+router.get("/api/books/remove/:id", (req, res) => {
   Books.findOneAndRemove({_id: req.params.id})
     .then(book => {
       res.json(book);
