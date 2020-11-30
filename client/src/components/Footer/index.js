4b8b3cdba5ff9a47d88d63f8fb0b1e974c01712b
Melinda Ivanov <jns.melinda@gmail.com>
2020-11-29 19:38:07 -0800

    adding basic footer

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+      <footer className="footer mt-auto py-3">
+        <div className="container">
+          <span className="text-muted">© 2020 Melinda Ivanov</span>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
--- client/src/pages/Saved/index.js
+++ client/src/pages/Saved/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../../components/Header";
+import Footer from "../../components/Footer";
 
 const Saved = () => {
   return (
     <div>
       <Header></Header>
       Saved
+      <Footer></Footer>
     </div>
   );
 }
