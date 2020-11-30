f3b1a82aa8e2e22254f59b21bff8f3623ad29636
Melinda Ivanov <jns.melinda@gmail.com>
2020-11-30 15:42:25 -0800

    getting books from db

--- client/src/pages/Saved/index.js
+++ client/src/pages/Saved/index.js
@@ -1,15 +1,39 @@
 import React from "react";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
+import Book from "../../components/Book";
+import API from "../../utils/API";
 
-const Saved = () => {
-  return (
-    <div>
-      <Header></Header>
-      Saved
-      <Footer></Footer>
-    </div>
-  );
+class Saved extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: []
+    };
+  }
+
+  componentDidMount() {
+      API.getBooks()
+        .then(res => this.setState({ items: res.data }))
+        .catch(err => console.log(err))
+  }
+
+  render() {
+    console.log(this.state.items);
+    return (
+      < div >
+        <Header></Header>
+        <ul>
+          {this.state.items.map(item => (
+            <Book
+              item={item}>
+            </Book>
+          ))}
+        </ul>
+        <Footer></Footer>
+      </div >
+    );
+  }
 }
 
 export default Saved;
