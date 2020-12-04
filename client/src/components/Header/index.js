e0da9ce1d5baec26e4c3112f2cb5e609c8bb5c18
Melinda Ivanov <jns.melinda@gmail.com>
2020-12-04 13:43:23 -0800

    styling header

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./index.css";
 
 function Header() {
   return (
     <div>
       <nav className="navbar navbar-light bg-light">
         <ul>
-          <li><Link to="search">Search</Link></li>
-          <li><Link to="saved">Saved</Link></li>
+          <li id="logo">Google Books</li>
+          <li><Link to="search" id="link">Search</Link></li>
+          <li><Link to="saved" id="link">Saved</Link></li>
         </ul>
       </nav>
     </div>
